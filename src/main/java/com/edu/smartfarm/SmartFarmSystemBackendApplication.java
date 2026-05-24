aa0daabae07b17bcdc0212bae0b120ae2aee6364
Hj <85759350@qq.com>
2026-05-24 13:42:37 +0800

    初始化

---
+++ src/main/java/com/edu/smartfarm/SmartFarmSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.smartfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFarmSystemBackendApplication.class, args);
+    }
+
+}
